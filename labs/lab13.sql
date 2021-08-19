0bdbdef7a077123fa46a21a1c8b1df4d1d89a396
Chachatzh <xiaoxinerw@gmail.com>
2021-08-19 17:49:37 +0900

    8/19

---
+++ labs/lab13.sql
@@ -0,0 +1,13 @@
+.read su19data.sql
+
+CREATE TABLE obedience AS
+  SELECT seven, instructor FROM students;
+
+CREATE TABLE smallest_int AS
+  SELECT time, smallest FROM students WHERE smallest > 2 ORDER BY smallest LIMIT 20;
+
+CREATE TABLE matchmaker AS
+  SELECT f.pet, f.song, f.color, s.color FROM students AS f, students AS s WHERE f.time < s.time AND f.pet = s.pet AND f.song = s.song;
+
+CREATE TABLE smallest_int_having AS
+  SELECT time, smallest FROM students GROUP BY smallest HAVING COUNT(smallest) = 1;
---
+++ labs/lab13_extra.sql
@@ -0,0 +1,12 @@
+.read lab13.sql
+
+CREATE TABLE su19favpets AS
+  SELECT pet, COUNT(*) AS count FROM students GROUP BY pet ORDER BY count DESC, pet LIMIT 10;
+
+
+CREATE TABLE su19dog AS
+  SELECT pet, count FROM su19favpets WHERE pet = "dog";
+
+
+CREATE TABLE obedienceimages AS
+  SELECT seven, instructor, COUNT(instructor) AS count FROM students WHERE seven = "7" GROUP BY instructor;
